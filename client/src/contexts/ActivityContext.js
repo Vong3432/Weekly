c5229afc2043e7eb34f54dff94aed6fe144f5b19
Vong3432 <vongnyuksoon2000@gmail.com>
2020-04-08 22:53:07 +0800

    Test

--- client/src/contexts/ActivityContext.js
+++ client/src/contexts/ActivityContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import moment from 'moment'
 import { ActivityReducer } from "../reducers/ActivityReducer";
 
@@ -8,14 +8,22 @@ export const ActivityContextProvider = props => {
 
     const [ activities, dispatch ] = useReducer(ActivityReducer, [], () => {
 
-        const authorized_user = localStorage.getItem('authorized_user');
-
-        if(!authorized_user) {
-            console.log('here?')
+        const authorized_user = localStorage.getItem('authorized_user');        
+        const cloud_data = localStorage.getItem('cloud_data');            
+        
+        if(!authorized_user) {            
             const localData = localStorage.getItem('user');
             return localData ? JSON.parse(localData) : [];   
         }     
+
+        else if(cloud_data) {            
+            return cloud_data ? JSON.parse(cloud_data) : [];
+        }
     })
+
+    useEffect(() => {
+        console.log(activities)
+    }, [activities])
 
     return (
         <ActivityContext.Provider value={{activities, dispatch}}>
