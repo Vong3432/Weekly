3568ad41dd7dce2445d0c3b78444a4f0dc976914
Vong3432 <vongnyuksoon2000@gmail.com>
2020-04-09 00:04:11 +0800

    Test

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ if(process.env.NODE_ENV !== 'production') {
 if(process.env.NODE_ENV === 'production') {
 
     // Test    
-    app.use(express.static('/client/build'));    
+    app.use(express.static('client/build'));    
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     })
 }
 
