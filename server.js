e9428b889baf2c5b6588fb896d59b644257c68e0
Vong3432 <vongnyuksoon2000@gmail.com>
2020-04-08 20:00:53 +0800

    Setup heroku

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ if(process.env.NODE_ENV !== 'production') {
     dotenv.config();
 }
 
+if(process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('/client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, './client/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build'))
-})
 
 // Configuring Port
 const port = process.env.PORT || 5000;
