5d74fb408418e5c8fcf9d38e2fe5f402688a3a7c
Vong3432 <vongnyuksoon2000@gmail.com>
2020-04-12 00:51:51 +0800

    Urgent Fix for GSAP plugin

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { gsap } from 'gsap'
+import { CSSPlugin } from 'gsap/CSSPlugin'
+
+gsap.registerPlugin(CSSPlugin);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
