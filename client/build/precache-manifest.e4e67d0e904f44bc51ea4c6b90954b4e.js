2b6f9f608f6ebbeddcb158a3acdd3f1a198c1c92
Vong3432 <vongnyuksoon2000@gmail.com>
2020-07-04 15:22:30 +0800

    Production ready

---
+++ client/build/precache-manifest.e4e67d0e904f44bc51ea4c6b90954b4e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd68e76f03b1d3eb797c27c1144bc231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "320e8ae496c76f935fe5",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "810dbe2e81f0864eddcc",
+    "url": "/static/css/main.03bb2918.chunk.css"
+  },
+  {
+    "revision": "320e8ae496c76f935fe5",
+    "url": "/static/js/2.40b051eb.chunk.js"
+  },
+  {
+    "revision": "16c96389d9b2ef44c7a2f419a560c40a",
+    "url": "/static/js/2.40b051eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810dbe2e81f0864eddcc",
+    "url": "/static/js/main.4f3ade48.chunk.js"
+  },
+  {
+    "revision": "c1635cb6a7e27499e9e4",
+    "url": "/static/js/runtime-main.bd831014.js"
+  },
+  {
+    "revision": "9a6fb6f5cd3aa4ab1adaaab1f693f266",
+    "url": "/static/media/Rubik-Bold.9a6fb6f5.ttf"
+  },
+  {
+    "revision": "c87313aa86b7caa31a9a0accaa584970",
+    "url": "/static/media/Rubik-Medium.c87313aa.ttf"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "/static/media/Rubik-Regular.b3d0902b.ttf"
+  },
+  {
+    "revision": "b2f96543a5d22e42455c3a34b9722192",
+    "url": "/static/media/moon.b2f96543.svg"
+  },
+  {
+    "revision": "0cfcc7b98ee517b8f1848bfabc08d426",
+    "url": "/static/media/sun.0cfcc7b9.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -55,10 +55,12 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
 app.use('/user', require('./routes/user/user'));
 app.use('/activity', require('./routes/user/activity'))
 
-app.use(express.static('client/build'));    
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-})
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    })
+}
 
 // Listen to port
 app.listen(port, () => {
